d7b39cea071ee0620fbb27c35fdc6aa4e134f064
Zach Hoeken <hoeken@gmail.com>
2026-04-20 23:44:09 -1000

    rgb controller design review changes

--- src/controllers/RGBController.h
+++ src/controllers/RGBController.h
@@ -27,25 +27,28 @@ class RGBControllerInterface : public BaseController
   public:
     RGBControllerInterface(YarrboardApp& app, const char* name) : BaseController(app, name) {}
 
-    uint8_t maxBrightness = 50;
+    void setMaxBrightness(uint8_t v) { _maxBrightness = v; }
+    uint8_t getMaxBrightness() const { return _maxBrightness; }
 
     virtual void setStatusColor(uint8_t r, uint8_t g, uint8_t b) = 0;
     virtual void setStatusColor(const CRGB& color) = 0;
-    virtual void setPixelColor(uint8_t c, uint8_t r, uint8_t g, uint8_t b) = 0;
-    virtual void setPixelColor(uint8_t c, const CRGB& color) = 0;
+    virtual void setPixelColor(uint16_t c, uint8_t r, uint8_t g, uint8_t b) = 0;
+    virtual void setPixelColor(uint16_t c, const CRGB& color) = 0;
+
+  protected:
+    uint8_t _maxBrightness = 50;
 };
 
 template <template <uint8_t, EOrder> class LED_TYPE, int DATA_PIN, EOrder COLOR_ORDER = GRB>
 class RGBController : public RGBControllerInterface
 {
   public:
-    RGBController(YarrboardApp& app, int numLeds) : RGBControllerInterface(app, "rgb")
+    RGBController(YarrboardApp& app, uint16_t numLeds) : RGBControllerInterface(app, "rgb")
     {
       _numLeds = numLeds;
       _leds = new CRGB[_numLeds];
 
       FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(_leds, _numLeds);
-      FastLED.clear();
       setStatusColor(CRGB::Blue);
       FastLED.show();
     }
@@ -63,12 +66,16 @@ class RGBController : public RGBControllerInterface
 
     void loop() override
     {
-      // 10hz refresh
-      if (millis() - lastRGBUpdateMillis > 1000) {
-        FastLED.setBrightness(maxBrightness * _cfg.getGlobalBrightness());
+      // 1hz refresh
+      if (millis() - _lastRGBUpdateMillis > 1000) {
         FastLED.show();
-        lastRGBUpdateMillis = millis();
+        _lastRGBUpdateMillis = millis();
       }
+    }
+
+    void updateBrightnessHook(float brightness) override
+    {
+      FastLED.setBrightness((uint8_t)(_maxBrightness * brightness));
     }
 
     void generateCapabilitiesHook(JsonVariant config) override
@@ -79,40 +86,36 @@ class RGBController : public RGBControllerInterface
     void setStatusColor(uint8_t r, uint8_t g, uint8_t b) override { setPixelColor(0, r, g, b); }
     void setStatusColor(const CRGB& color) override { setPixelColor(0, color); }
 
-    void setPixelColor(uint8_t c, uint8_t r, uint8_t g, uint8_t b) override
+    void setPixelColor(uint16_t c, uint8_t r, uint8_t g, uint8_t b) override
     {
-      // out of bounds?
       if (c >= _numLeds)
         return;
 
       _leds[c].setRGB(r, g, b);
-      FastLED.setBrightness(maxBrightness * _cfg.getGlobalBrightness());
-
-      if (millis() - lastRGBUpdateMillis > 100) {
-        FastLED.show();
-        lastRGBUpdateMillis = millis();
-      }
+      _showIfReady();
     }
 
-    void setPixelColor(uint8_t c, const CRGB& color) override
+    void setPixelColor(uint16_t c, const CRGB& color) override
     {
-      // out of bounds?
       if (c >= _numLeds)
         return;
 
       _leds[c] = color;
-      FastLED.setBrightness(maxBrightness * _cfg.getGlobalBrightness());
-
-      if (millis() - lastRGBUpdateMillis > 100) {
-        FastLED.show();
-        lastRGBUpdateMillis = millis();
-      }
+      _showIfReady();
     }
 
   private:
-    unsigned long lastRGBUpdateMillis = 0;
+    void _showIfReady()
+    {
+      if (millis() - _lastRGBUpdateMillis > 100) {
+        FastLED.show();
+        _lastRGBUpdateMillis = millis();
+      }
+    }
+
+    unsigned long _lastRGBUpdateMillis = 0;
     CRGB* _leds = nullptr;
-    int _numLeds;
+    uint16_t _numLeds;
 };
 
 #endif /* !YARR_RGB_H */
